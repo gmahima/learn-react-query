576f61fdf9e0b7c7111130a76fadc7f873104764
gmahima <gmahima@Gs-MacBook-Air.local>
2023-11-04 23:38:17 +0530

    use isSuccess for type safety

--- app/poke/page.tsx
+++ app/poke/page.tsx
@@ -4,10 +4,12 @@ import {PokeAPI} from "pokeapi-types";
 import {
   QueryClient,
   QueryClientProvider,
+  keepPreviousData,
   useQuery,
+  useQueryClient,
 } from "@tanstack/react-query";
 import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 const fetchPokemon = async (name = "ditto"): Promise<PokeAPI.Pokemon> => {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
   return response.json();
@@ -57,50 +59,63 @@ const PokeDetail: React.FC<PokeDetailProps> = ({name}) => {
 
 const PokeList: React.FC<PokeListProps> = ({page, setPage}) => {
   const [selectedPokemon, setSelectedPokemon] = React.useState("");
-  const {data: pokeListData} = useQuery({
+  const queryClient = useQueryClient();
+  useEffect(() => {
+    const nextPage = page + 1;
+    queryClient.prefetchQuery({
+      queryKey: ["list", nextPage],
+      queryFn: () => fetchPokeList(page + 1),
+    });
+  }, [page, queryClient]);
+  const pokeListData = useQuery({
     queryKey: ["list", page],
     queryFn: () => fetchPokeList(page),
+    placeholderData: keepPreviousData,
   });
-  if (!pokeListData) {
-    return "Loading...";
-  }
-  return (
-    <div>
-      <h2 className="text-center">Pokemon</h2>
-      <div className="flex flex-col">
-        <ul>
-          {pokeListData.results.map((pokemon) => (
-            <li
-              key={pokemon.name}
-              className={`mt-2 bg-gray-100 ${
-                pokemon.name === selectedPokemon && "bg-gray-200"
-              }`}
-              onClick={() => setSelectedPokemon(pokemon.name)}
-            >
-              {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
-            </li>
-          ))}
-          <div className="flex justify-between border border-gray-300 rounded mt-4">
-            <button
-              onClick={() => setPage((page) => page - 1)}
-              disabled={!pokeListData.previous}
-              className={`${!pokeListData.previous && "text-gray-200"}`}
-            >
-              &lt; Prev
-            </button>
-            <button
-              disabled={!pokeListData.next}
-              onClick={() => setPage((page) => page + 1)}
-              className={`${!pokeListData.next && "text-gray-200"}`}
-            >
-              Next &gt;
-            </button>
-          </div>
-          {selectedPokemon && <PokeDetail name={selectedPokemon}></PokeDetail>}
-        </ul>
+  if (pokeListData.isSuccess) {
+    return (
+      <div>
+        <h2 className="text-center">Pokemon</h2>
+        <div className="flex flex-col">
+          <ul>
+            {pokeListData.data.results.map((pokemon) => (
+              <li
+                key={pokemon.name}
+                className={`px-2 py-4 mt-2 bg-gray-100 hover:bg-gray-200 ${
+                  pokemon.name === selectedPokemon &&
+                  "bg-gray-300 hover:bg-gray-300"
+                }`}
+                onClick={() => setSelectedPokemon(pokemon.name)}
+              >
+                {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+              </li>
+            ))}
+            <div className="px-2 flex justify-between border border-gray-300 rounded mt-4">
+              <button
+                onClick={() => setPage((page) => page - 1)}
+                disabled={!pokeListData.data.previous}
+                className={`${!pokeListData.data.previous && "text-gray-200"}`}
+              >
+                &lt; Prev
+              </button>
+              <button
+                disabled={!pokeListData.data.next}
+                onClick={() => setPage((page) => page + 1)}
+                className={`${!pokeListData.data.next && "text-gray-200"}`}
+              >
+                Next &gt;
+              </button>
+            </div>
+            {selectedPokemon && (
+              <PokeDetail name={selectedPokemon}></PokeDetail>
+            )}
+          </ul>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return <em>loading...</em>;
+  }
 };
 function Poke() {
   const [page, setPage] = useState(0);
